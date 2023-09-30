acb502ef22886fad44c9a87e7113c9da6b951158
Gustavo Avila <gustavogavila01@gmail.com>
2023-09-30 15:19:51 -0300

    Initial project

---
+++ src/main/java/com/gus/strategyreports/StrategyReportsApplication.java
@@ -0,0 +1,13 @@
+package com.gus.strategyreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategyReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gus/strategyreports/StrategyReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gus.strategyreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
